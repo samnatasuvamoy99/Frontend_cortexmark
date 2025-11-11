1c3bae3b67f0eb0b31638c668a353e5399581635
suavmoy samanta <suvamoysamanta907@gmail.com>
2025-11-11 17:20:43 +0530

    Frontend update

--- src/icons/Open_Link.tsx
+++ src/icons/Open_Link.tsx
@@ -1,18 +1,20 @@
 export function Openlogo() {
   return (
+    // 
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6 "
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5A2.25 2.25 0 0 0 6.75 19.5h7.5A2.25 2.25 0 0 0 16.5 17.25v-7.5A2.25 2.25 0 0 0 14.25 7.5h-.75m0-3-3-3m0 0-3 3m3-3v11.25m6-2.25h.75A2.25 2.25 0 0 1 20.25 14.25v7.5A2.25 2.25 0 0 1 18 24H10.5A2.25 2.25 0 0 1 8.25 21.75v-.75"
-      />
-    </svg>
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth="1.5"
+  stroke="blue"
+  className="size-6"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12"
+  />
+</svg>
+
   );
 }

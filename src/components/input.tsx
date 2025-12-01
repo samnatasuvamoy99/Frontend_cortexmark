e55db2c6d0f6b70899d2adef8824f7568aa52007
suavmoy samanta <suvamoysamanta907@gmail.com>
2025-12-02 00:33:52 +0530

    small changes

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,8 +6,19 @@ type InputProps = {
 };
 
 export function Input({ placeholder , reference ,type }:InputProps){
-        return <input ref={reference} placeholder={placeholder} type={type} className="px-8 py-2 w-80 bg-gray-200 border-1 border-gray-400 rounded mb-4 hover:border-purple-600 hover:border-2 shadow-md"> 
-                </input>
         
+        return (
+  <input
+    ref={reference}
+    placeholder={placeholder}
+    type={type}
+    className="px-8 py-2 w-80 bg-gray-200 border border-gray-400 rounded mb-4 shadow-md 
+               hover:border-purple-600 
+                hover:border-1.5
+               focus:border-purple-600 
+        focus:outline-none"
+  />
+);
+
         
 }

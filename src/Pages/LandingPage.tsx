b6a772384bb0170a6f84897e9fc5b44d0ea7c5e5
suavmoy samanta <suvamoysamanta907@gmail.com>
2026-06-08 21:42:29 +0530

    small changes

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -17,7 +17,7 @@ interface Platform {
 
 
 const DOWNLOAD_URL =
-  "https://drive.usercontent.google.com/download?id=1Ak_12-JNL4aMvh7vl0sDUCGQNwOwbUxL&export=download&authuser=0";
+  "https://drive.usercontent.google.com/download?id=15A7GCAYLjsFKDFeYDNA0XEc50wBUnd8d&export=download"
 
   const DOWNLOAD_URL2 ="https://github.com/samnatasuvamoy99/Cortex_Mark/blob/main/Chrome_Extension_CortexMark/README.md";
 
